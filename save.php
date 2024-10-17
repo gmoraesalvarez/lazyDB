3dd7c2a6c78e3ec0cb71db337bbc429999768f63
Guilherme de Moraes Alvarez <moraes.alvarez@gmail.com>
2024-10-17 13:51:20 -0300

    Update save.php

--- save.php
+++ save.php
@@ -1,19 +1,20 @@
 <?php
-session_start();
+//session_start();
+//if ($_SESSION['nat'] != 'on') {header( "Location: $url" );}
 echo "save is reached\n";
 if (file_exists('dados') == false){
   mkdir('dados');
 }
-
 if (isset($_GET['save'])){
     echo "save is a get<br>";
 }
-if (isset($_POST['dados'])){
-    //echo "a post is acts\n";
-    //echo  $_POST['acts'];
-    echo $_POST['dados'];
-    if (file_put_contents('dados/'.$_POST['save'].'.txt',$_POST['dados']) == false){
-    	echo 'falha';
+$save_token = 'somecharactersequenceyoushouldnthardcode';
+if (isset($_POST['token'])) { echo "post-token = ".$_POST['token']."\n"; } else { echo "no token sent\n"; }
+if ($_POST['token'] == $save_token){
+  if (isset($_POST['dados'])){
+    $dados=$_POST['dados'];    
+    if (file_put_contents('dados/'.$_POST['save'].'.txt',rawurldecode($_POST['dados']).PHP_EOL) == false){
+      echo 'falha';
     }else {echo 'saved '.$_POST['save'];}
-  
-}
+  }
+} else { echo 'na, na, nã'; }
